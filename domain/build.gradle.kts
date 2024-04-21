275b8008261e62304e3fc6e6baa53343902302b6
junseo511 <prime54@g.skku.edu>
2024-04-21 21:11:11 +0900

    [chore] 도메인 힐트 제거

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,8 +7,3 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
-
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.hilt)
-}
--- domain/src/main/java/org/sopt/now/advanced/team2android/domain/usecase/GetDummyUserListUseCase.kt
+++ domain/src/main/java/org/sopt/now/advanced/team2android/domain/usecase/GetDummyUserListUseCase.kt
@@ -2,9 +2,7 @@ package org.sopt.now.advanced.team2android.domain.usecase
 
 import org.sopt.now.advanced.team2android.domain.entity.UserEntity
 import org.sopt.now.advanced.team2android.domain.repository.DummyRepository
-import javax.inject.Singleton
 
-@Singleton
 class GetDummyUserListUseCase(private val dummyRepository: DummyRepository) {
     suspend operator fun invoke(page: Int = 1): Result<List<UserEntity>> =
         dummyRepository.getDummyUserList(page = page)
