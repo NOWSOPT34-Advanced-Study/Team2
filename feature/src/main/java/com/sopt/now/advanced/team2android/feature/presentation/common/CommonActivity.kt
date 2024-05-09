74adad35d08e010307ff902126b40cd2b9b12132
junseo511 <prime54@g.skku.edu>
2024-05-10 04:14:25 +0900

    [feat] #8 시작 액티비티 변경

---
+++ feature/src/main/java/com/sopt/now/advanced/team2android/feature/presentation/common/CommonActivity.kt
@@ -0,0 +1,63 @@
+package com.sopt.now.advanced.team2android.feature.presentation.common
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
+import com.sopt.now.advanced.team2android.feature.R
+import com.sopt.now.advanced.team2android.feature.databinding.ActivityCommonBinding
+import com.sopt.now.advanced.team2android.feature.presentation.signIn.SignInFragmentDirections
+import dagger.hilt.android.AndroidEntryPoint
+import org.sopt.now.advanced.team2android.core.ui.base.BindingActivity
+
+@AndroidEntryPoint
+class CommonActivity :
+    BindingActivity<ActivityCommonBinding>({ ActivityCommonBinding.inflate(it) }) {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupNavHost()
+    }
+
+    private fun setupNavHost() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_common_nav_host) as NavHostFragment?
+                ?: return
+        navController = navHostFragment.navController
+        setupDestinationListener()
+        setupBottomNavigation()
+    }
+
+    private fun setupDestinationListener() {
+        val mainFragmentList = listOf(
+            R.id.homeFragment,
+            R.id.searchFragment,
+            R.id.myPageFragment
+        )
+
+        navController.addOnDestinationChangedListener { _: NavController?, destination: NavDestination, _: Bundle? ->
+            if (destination.id in mainFragmentList) {
+                binding.bnvMain.visibility = View.VISIBLE
+            } else {
+                binding.bnvMain.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bnvMain.setupWithNavController(navController)
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            NavigationUI.onNavDestinationSelected(item, navController)
+            Snackbar.make(binding.root, item.title.toString(), Snackbar.LENGTH_SHORT).show()
+            return@setOnItemSelectedListener true
+        }
+    }
+}
