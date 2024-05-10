4a810fa09c8fdabbf1abeb80914975ca9f54c9cb
arinming <jyandysys1@naver.com>
2024-05-10 18:35:11 +0900

    [feat] #10 종속성 추가

--- feature/src/main/java/com/sopt/now/advanced/team2android/feature/ui/signIn/SignInFragment.kt
+++ feature/src/main/java/com/sopt/now/advanced/team2android/feature/ui/signIn/SignInFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.advanced.team2android.feature.databinding.FragmentSignInBinding
+import com.sopt.now.advanced.team2android.feature.ui.signUp.SignUpFragmentDirections
 import org.sopt.now.advanced.team2android.core.ui.base.BindingFragment
 
 class SignInFragment :
@@ -12,17 +13,30 @@ class SignInFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupSignInBtnClickListener()
         setupSignUpBtnClickListener()
     }
 
-    private fun setupSignUpBtnClickListener() {
+    private fun setupSignInBtnClickListener() {
         binding.btnSignIn.setOnClickListener {
             navigateToHome()
         }
     }
 
+    private fun setupSignUpBtnClickListener() {
+        binding.btnSignUp.setOnClickListener {
+            navigateToSignUp()
+        }
+    }
+
+
     private fun navigateToHome() {
         Snackbar.make(binding.root, "로그인 버튼 클릭", Snackbar.LENGTH_SHORT).show()
         findNavController().navigate(SignInFragmentDirections.actionSignInToHome())
+    }
+
+    private fun navigateToSignUp() {
+        Snackbar.make(binding.root, "회원가입 버튼 클릭", Snackbar.LENGTH_SHORT).show()
+        findNavController().navigate(SignInFragmentDirections.actionSignInToSignUp())
     }
 }
